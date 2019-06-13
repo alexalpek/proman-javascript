d43dbeb11f5813319af002dc7081b864f1411147
Alex Alpek <alexalpek@gmail.com>
2019-06-13 15:57:12 +0200

    minor bug and typo fixes

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,17 +1,17 @@
 // It uses data_handler.js to visualize elements
-import { dataHandler } from "./data_handler.js";
+import {dataHandler} from "./data_handler.js";
 
 export let dom = {
     init: function () {
         // This function should run once, when the page is loaded.
     },
-    clearBoardContainer : function(){
+    clearBoardContainer: function () {
         const boardContainer = document.querySelector('#boards');
         boardContainer.innerHTML = '';
     },
     loadBoards: function () {
         // retrieves boards and makes showBoards called
-        dataHandler.getBoards(function(boards){
+        dataHandler.getBoards(function (boards) {
             dom.clearBoardContainer();
             dom.showBoards(boards);
         });
@@ -21,7 +21,7 @@ export let dom = {
         // it adds necessary event listeners also
 
         let boardList = '';
-        for(let board of boards){
+        for (let board of boards) {
             boardList += `
                 <section class="board">
                 <div class="board-header"><span class="board-title">${board.title}</span>
@@ -45,7 +45,9 @@ export let dom = {
 
         const toggleButtons = document.querySelectorAll('.board-toggle');
         for (let button of toggleButtons) {
-            button.addEventListener('click', dom.toggleBoard)
+            button.addEventListener('click', function () {
+                dom.toggleBoard()
+            })
         }
     },
     loadCards: function (boardId) {
@@ -68,10 +70,11 @@ export let dom = {
         console.log(statusIds);
         for (let card of cards) {
             if (statusIds.indexOf("status_" + `${card.status_id}`)) {
-                let parentStatusId = statusIds.indexOf(card.status_id)
+                let parentStatusId = statusIds.indexOf(card.status_id);
                 console.log(parentStatusId);
 
-            }}
+            }
+        }
 
 
     },
@@ -85,7 +88,7 @@ export let dom = {
             dom.clearStatusContainer(boardId);
             dom.showStatuses(statuses, boardId);
             dom.loadCards(boardId)
-            });
+        });
     },
     showStatuses: function (statuses, boardId) {
         let statusList = '';
@@ -97,17 +100,17 @@ export let dom = {
                     </div>
                 </div>     
             `;
-        const statusContainer = document.getElementById(boardId);
+            const statusContainer = document.getElementById(boardId);
             statusContainer.innerHTML = statusList;
-        }},
+        }
+    },
     toggleBoard: function () {
         const button = event.currentTarget,
-              boardId = button.closest(".board").lastElementChild.id;
+            boardId = button.closest(".board").lastElementChild.id;
         if (button.innerHTML === "AWOKEN") {
             button.innerHTML = "WAKE ME UP";
             dom.clearStatusContainer(boardId)
-        }
-        else {
+        } else {
             button.innerHTML = "AWOKEN";
             dom.loadStatuses(boardId);
         }
